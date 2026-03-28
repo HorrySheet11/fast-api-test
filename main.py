7e23c4a5d21b72f5b7e6213337608e9af50425c3
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2026-03-28 20:16:27 +0800

    refactor: added router folder and moved routes to index.py

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI
+from routers import items
 
 app = FastAPI()
 
-class Item(BaseModel):
-  text: str = None
-  is_done: bool = False
-
-items = []
-
+app.include_router(
+  items.router
+)
+#FIXME: ImportError: attempted relative import with no known parent package
 @app.get("/")
 async def root():
   return {"message": "Hello World"}
 
-@app.get("/items/{item_id}", response_model=Item)
-async def read_item(item_id: int):
-  if item_id >= len(items):
-    raise HTTPException(status_code=404, detail=f"Item {item_id} not found") 
-  else:
-    return items[item_id]
+# @app.get("/items/{item_id}", response_model=Item)
+# async def read_item(item_id: int):
+#   if item_id >= len(items):
+#     raise HTTPException(status_code=404, detail=f"Item {item_id} not found") 
+#   else:
+#     return items[item_id]
 
-@app.post("/items/")
-async def create_item(item: Item):
-  items.append(item)
-  return items
+# @app.post("/items/")
+# async def create_item(item: Item):
+#   items.append(item)
+#   return items
 
-@app.get("/items")
-async def read_items():
-  return items
+# @app.get("/items")
+# async def read_items():
+#   return items
 
-@app.get("/items/", response_model=Item)
-async def list_items(limit: int = 10):
-  return items[0:limit]
+# @app.get("/items/", response_model=Item)
+# async def list_items(limit: int = 10):
+#   return items[0:limit]
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-  if item_id >= len(items):
-    raise HTTPException(status_code=404, detail=f"Item {item_id} not found") 
-  else:
-    items[item_id] = item
-    return items
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#   if item_id >= len(items):
+#     raise HTTPException(status_code=404, detail=f"Item {item_id} not found") 
+#   else:
+#     items[item_id] = item
+#     return items
---
+++ routers/items.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+
+items = [{"text": "apple", "is_done": True}, {"text": "banana", "is_done": False}]
+class Item(BaseModel):
+  text: str
+  is_done: bool = False
+
+
+router = APIRouter( 
+  prefix="/items",
+  tags=["items"],
+  responses={404: {"description": "Not found"}})
+
+@router.get("/{item_id}", response_model=Item)
+async def read_item(item_id: int):
+  if item_id >= len(items):
+    raise HTTPException(status_code=404, detail=f"Item {item_id} not found") 
+  else:
+    return items[item_id]
+
+@router.post("/")
+async def create_item(item: Item):
+  items.append(item)
+  return items
+
+@router.get("/")
+async def read_items():
+  return items
+
+@router.put("/{item_id}")
+async def update_item(item_id: int, item: Item):
+  if item_id >= len(items):
+    raise HTTPException(status_code=404, detail=f"Item {item_id} not found") 
+  else:
+    items[item_id] = item
+    return items
