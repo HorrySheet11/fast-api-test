b39377a981e4236f3e4cd9a732076bcbeb9e25ce
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2026-03-31 20:21:52 +0800

    fix: fixed python interpreter
    refactor: added dict for items

--- main.py
+++ main.py
@@ -6,35 +6,7 @@ app = FastAPI()
 app.include_router(
   items.router
 )
-#FIXME: ImportError: attempted relative import with no known parent package
 @app.get("/")
 async def root():
   return {"message": "Hello World"}
 
-# @app.get("/items/{item_id}", response_model=Item)
-# async def read_item(item_id: int):
-#   if item_id >= len(items):
-#     raise HTTPException(status_code=404, detail=f"Item {item_id} not found") 
-#   else:
-#     return items[item_id]
-
-# @app.post("/items/")
-# async def create_item(item: Item):
-#   items.append(item)
-#   return items
-
-# @app.get("/items")
-# async def read_items():
-#   return items
-
-# @app.get("/items/", response_model=Item)
-# async def list_items(limit: int = 10):
-#   return items[0:limit]
-
-# @app.put("/items/{item_id}")
-# async def update_item(item_id: int, item: Item):
-#   if item_id >= len(items):
-#     raise HTTPException(status_code=404, detail=f"Item {item_id} not found") 
-#   else:
-#     items[item_id] = item
-#     return items
--- routers/items.py
+++ routers/items.py
@@ -12,17 +12,17 @@ router = APIRouter(
   tags=["items"],
   responses={404: {"description": "Not found"}})
 
+@router.post("/")
+async def create_item(item: Item):
+  items.append(dict(item))
+  return items
+
 @router.get("/{item_id}", response_model=Item)
 async def read_item(item_id: int):
   if item_id >= len(items):
     raise HTTPException(status_code=404, detail=f"Item {item_id} not found") 
   else:
     return items[item_id]
-
-@router.post("/")
-async def create_item(item: Item):
-  items.append(item)
-  return items
 
 @router.get("/")
 async def read_items():
@@ -33,5 +33,5 @@ async def update_item(item_id: int, item: Item):
   if item_id >= len(items):
     raise HTTPException(status_code=404, detail=f"Item {item_id} not found") 
   else:
-    items[item_id] = item
+    items[item_id] = item.dict()
     return items
