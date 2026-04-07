e5e3ca74d7834e7fe11fe3e418db305d0bed4ae3
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2026-04-07 12:49:08 +0800

    feat: created add item logic to db
    fix: added conditional db connection in backend
    feat: added delete item logic to db
    feat: added modal to add and inspect item using MUI

--- fastAPI-backend/routers/items.py
+++ fastAPI-backend/routers/items.py
@@ -14,9 +14,19 @@ router = APIRouter(
   tags=["items"],
   responses={404: {"description": "Not found"}})
 
+
+#FIXME: Check if db is connected
+async def is_db_connected() -> bool:
+    try:
+        await db.execute_raw('SELECT 1')
+        return True
+    except Exception:
+        return False
+
 @router.get("/")
 async def read_items():
-  await db.connect()
+  if await is_db_connected() == False:
+      await db.connect()
   print("read_items")
   items = await db.items.find_many()
   await db.disconnect()
@@ -24,7 +34,8 @@ async def read_items():
 
 @router.post("/")
 async def create_item(item: Item):
-  await db.connect()
+  if await is_db_connected() == False:
+      await db.connect()
   data = await db.items.create(
     data={
       "name": item.text,
@@ -36,7 +47,8 @@ async def create_item(item: Item):
 
 @router.get("/{item_id}")
 async def read_item(item_id : int):
-  await db.connect()
+  if await is_db_connected() == False:
+      await db.connect()
   length = await db.items.count()
   if item_id < 0 or item_id > length:
     raise HTTPException(status_code=404, detail="Item not found")
@@ -48,7 +60,8 @@ async def read_item(item_id : int):
 
 @router.put("/{item_id}")
 async def update_item(item_id: int, item: Item):
-  await db.connect()
+  if await is_db_connected() == False:
+      await db.connect()
   res = await db.items.update(
     where={"id": item_id},
     data={
@@ -61,7 +74,8 @@ async def update_item(item_id: int, item: Item):
 
 @router.delete("/{item_id}")
 async def delete_item(item_id: int):
-  await db.connect()
+  if await is_db_connected() == False:
+      await db.connect()
   res = await db.items.delete(
     where={"id": item_id}
   )
--- fastAPI-react/src/App.jsx
+++ fastAPI-react/src/App.jsx
@@ -7,9 +7,12 @@ import { useEffect, useState } from "react";
 import API from "./utils/axios";
 
 function App() {
-	const [items, setItems] = useState([]);
+	const [items, setItems] = useState();
 	const [item, setItem] = useState();
+	const [addItem, setAddItem] = useState("");
+	const [checked, setChecked] = useState(false);
 	const [dialogOpen, setDialogOpen] = useState(false);
+	const [itemDialogOpen, setItemDialogOpen] = useState(false);
 
 	useEffect(() => {
 		try {
@@ -36,26 +39,70 @@ function App() {
 		}
 	};
 
-	useEffect(() => {
-		console.log(item);
-	}, [item]);
-
-	useEffect(() => {
-		console.log(items);
-	}, [items]);
-
-	function handleClose() {
-		setDialogOpen(false);
+	function handleClose(item) {
+		if (item) {
+			setItemDialogOpen(false);
+		} else {
+			setDialogOpen(false);
+		}
 		setItem();
+	}
+
+	function handleDelete() {
+		try {
+			async function deleteItem(id) {
+				await API.delete(`/items/${id}`);
+			}
+			deleteItem(item.id);
+		} catch (error) {
+			alert(error);
+		}
+		handleClose();
+	}
+
+	async function handleSubmit() {
+		try {
+				const response = await API.post(
+					"/items",
+					{
+						text: addItem,
+						is_done: checked,
+					},
+					{
+						headers: {
+							"Content-Type": "application/json",
+						},
+					},
+				);
+				console.log(response.data);
+		} catch (error) {
+			alert(error);
+		} finally {
+			handleClose("add");
+			setAddItem("");
+			setChecked(false);
+			const response = await API.get("/items");
+			setItems(response.data);
+		}
+	}
+
+	function handleNameChange(e) {
+		setAddItem(e.target.value);
 	}
 
 	return (
 		<div className="p-10 h-screen w-full flex flex-col items-center gap-20">
-			<button type="button" className="p-2 bg-slate-400 rounded w-50 center">
+			<button
+				type="button"
+				className="p-2 bg-slate-400 rounded w-50 center"
+				onClick={() => setItemDialogOpen(true)}
+			>
 				Add Item
 			</button>
-			{!items || items.length === 0 ? (
+			{!items ? (
 				<h1>Getting items...</h1>
+			) : items.length === 0 ? (
+				<h1>No items</h1>
 			) : (
 				<div className="items-div flex flex-row flex-wrap gap-5 w-full justify-center items-center">
 					{items.map((item) => (
@@ -70,28 +117,57 @@ function App() {
 					))}
 				</div>
 			)}
-			{/* TODO: continue dialog */}
 			<Dialog
 				open={dialogOpen}
 				onClose={() => handleClose()}
 				className="w-200 h-200 mx-auto"
 			>
-				{
-					!item  ? (
-						<DialogTitle>Loading...</DialogTitle>
-					) : (
-						<>
-							<DialogTitle>{item.name}</DialogTitle>
-							<DialogContent>
-								<p>Completed: {item.isDone ? "True" : "False"}</p>
-							</DialogContent>
-						</>
-					)
-					/* <DialogActions>
-					<Button>Cancel</Button>
-					<Button>Ok</Button>
-				</DialogActions> */
-				}
+				{!item ? (
+					<DialogTitle>Loading...</DialogTitle>
+				) : (
+					<>
+						<DialogTitle>{item.name}</DialogTitle>
+						<DialogContent>
+							<p>Completed: {item.isDone ? "True" : "False"}</p>
+						</DialogContent>
+						<DialogActions>
+							<Button onClick={() => handleDelete(item.id)}>Delete</Button>
+							<Button>Back</Button>
+						</DialogActions>
+					</>
+				)}
+			</Dialog>
+			<Dialog open={itemDialogOpen} onClose={() => handleClose("add")}>
+				<DialogTitle>Add Item</DialogTitle>
+				<DialogContent>
+					<label htmlFor="item">
+						Item name:<> </>
+						<input
+							type="text"
+							name="item"
+							value={addItem}
+							placeholder="Enter item here"
+							className="p-1 bg-slate-400 rounded"
+							onChange={(e) => handleNameChange(e)}
+						/>
+					</label>
+					<br />
+					<label htmlFor="done">
+						Completed:<> </>
+						<input
+							type="checkbox"
+							name="done"
+							id="done"
+							checked={checked}
+							className="p-1 bg-slate-400 rounded"
+							onChange={() => setChecked(!checked)}
+						/>
+					</label>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={() => handleClose("add")}>Cancel</Button>
+					<Button onClick={() => handleSubmit()}>Submit</Button>
+				</DialogActions>
 			</Dialog>
 		</div>
 	);
